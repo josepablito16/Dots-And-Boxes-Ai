3edc5e0cb981a29b7561820d82ba572603588b18
josepablito16 <josepablocif16@gmail.com>
2020-05-30 00:23:45 -0600

    Se generan tiros simulados, se verifican si hay cuadrados o no

--- Test/generarArbolTest.py
+++ Test/generarArbolTest.py
@@ -1,14 +1,23 @@
-h=[0, 0, 99, 0, 0, 99]
+import random
 
-v=[0, 0, 0, 99, 99, 99]
+h=[0, 0,0, 0, 0, 99]
+
+v=[0,99, 0,0, 99, 99]
 
 N = 3 #cantidad de puntos en una linea
 
-posicionesLibresH=[]
-def getPosicionesLibreH():
-	for i in range(len(h)):
-		if(h[i]==99):
-			posicionesLibresH.append(i)
+YO=1 #Jugadas positivas
+OPONENTE=2 #Jugadas negativas
+
+EMPTY=99
+FILLED=0
+
+def getPosicionesLibres(lista):
+	posicionesLibres=[]
+	for i in range(len(lista)):
+		if(lista[i]==99):
+			posicionesLibres.append(i)
+	return posicionesLibres
 		
 '''
 def simularMiTiro():
@@ -22,34 +31,185 @@ getPosicionesLibreH()
 simularMiTiro()
 '''
 
+def buscarTiroCuadrado(tableroH,tableroV):
+	posicionesLibresH=getPosicionesLibres(tableroH)
+	posicionesLibresV=getPosicionesLibres(tableroV)
 
-punteoTurno = 0
-acumulador = 0
-contador = 0
-for x in range(0, N * (N - 1)):
-	if((x+1) % N) != 0:
-		if h[x] == 0 and h[x+1] == 0 and v[contador + acumulador] == 0 and v[contador + acumulador + 1] == 0:
-			punteoTurno = punteoTurno + 1
-			print("Hay cuadrado")
-			h[x]=1
-		
-		acumulador = acumulador + N
-	else:
-		contador = contador + 1
+	posiblesTiros=[]
+
+	for pos in posicionesLibresH:
+		posiblesTiros.append([0,pos])
+
+	for pos in posicionesLibresV:
+		posiblesTiros.append([1,pos])
+
+	print(posiblesTiros)
+	
+
+	for turno in posiblesTiros:
+
+		punteoAntesTurno=0
+		punteoTurno=0
+
 		acumulador = 0
+		contador = 0
 
-print(h)
-print(v)
-'''
-for x in range(0, N * (N - 1)):
-	if((x+1) % N) != 0:
-		if h[x] == 1 and h[x+1] == 1 and v[contador + acumulador] == 1 and v[contador + acumulador + 1] == 1:
-			punteoTurno = punteoTurno + 1
-			print("Hay cuadrado")
-		
-		acumulador = acumulador + N
-	else:
-		contador = contador + 1
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoAntesTurno = punteoAntesTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(turno[0]==0):
+			#Horizontal
+			tableroH[turno[1]]=FILLED
+		else:
+			#Vertical
+			tableroV[turno[1]]=FILLED
+
+
 		acumulador = 0
+		contador = 0
 
-'''
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoTurno = punteoTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(punteoAntesTurno<punteoTurno):
+
+			print("Cuadrado Interno")
+
+			return turno
+
+		else:
+
+			if(turno[0]==0):
+				#Horizontal
+				tableroH[turno[1]]=EMPTY
+			else:
+				#Vertical
+				tableroV[turno[1]]=EMPTY
+
+		return random.choice(posiblesTiros)
+
+
+
+
+
+def jugarSimulado(tableroH,tableroV,turno,jugador):
+	repetirTurno=True
+	contador=0
+
+	while repetirTurno:
+
+		if(contador!=0):
+			print("Se podria simular otro tiro")
+			turno=buscarTiroCuadrado(tableroH.copy(),tableroV.copy())
+			print(turno)
+			
+		else:
+			contador+=1
+
+		punteoAntesTurno=0
+		punteoTurno=0
+
+		acumulador = 0
+		contador = 0
+
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoAntesTurno = punteoAntesTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(turno[0]==0):
+			#Horizontal
+			tableroH[turno[1]]=FILLED
+		else:
+			#Vertical
+			tableroV[turno[1]]=FILLED
+
+
+		acumulador = 0
+		contador = 0
+
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoTurno = punteoTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(punteoAntesTurno<punteoTurno):
+			print()
+			print("Cuadrado")
+			print([tableroH,tableroV])
+			print()
+			repetirTurno=True
+
+			if(jugador==1):
+				#Positivos
+				if(punteoTurno - punteoAntesTurno==1):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=1
+					else:
+						#Vertical
+						tableroV[turno[1]]=1
+
+				elif(punteoTurno - punteoAntesTurno==2):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=2
+					else:
+						#Vertical
+						tableroV[turno[1]]=2
+
+			else:
+				#Negativo
+				if(punteoTurno - punteoAntesTurno==1):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=-1
+					else:
+						#Vertical
+						tableroV[turno[1]]=-1
+
+				elif(punteoTurno - punteoAntesTurno==2):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=-2
+					else:
+						#Vertical
+						tableroV[turno[1]]=-2
+		
+		else:
+			repetirTurno=False
+
+
+
+	return tableroH,tableroV
+
+
+print(jugarSimulado(h.copy(),v.copy(),[1,1],YO))
