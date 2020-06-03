5048105dae2e86fe90128cf848d3fdb1ab0a4ae8
josepablito16 <josepablocif16@gmail.com>
2020-06-03 01:00:56 -0600

    Se agrega carpeta de entrega final

---
+++ con MiniMax/Entrega/dotsAndBoxes.py
@@ -0,0 +1,125 @@
+""" 
+Jose Cifuentes
+
+"""
+import socketio
+import sys
+import random
+from tree_parser import *
+from alphaBeta import *
+
+sio = socketio.Client()
+
+YO=0
+OPONENTE=0
+
+class DotsAndBoxes:
+	def __init__(self,username,tid):
+		self.name=username
+		self.tournamentId=tid
+		self.playerId=0
+		self.gameId=""
+		self.board=[]
+		
+
+		print(self.name)
+		print(self.tournamentId)
+
+	def createBoard(self):
+		self.horizontal=[
+
+		]
+		self.Vertical=[]
+
+	def restart(self):
+		self.playerId=0
+		self.gameId=""
+		self.board=[]
+		
+
+
+@sio.on('connect')
+def onConnect():
+    print('Connection Successfully')
+    sio.emit('signin', {
+        'user_name': Dot.name,
+        'tournament_id': Dot.tournamentId,
+        'user_role': 'player'
+    })
+
+@sio.on('ready')
+def onReady(server):
+    print('Ready')
+    print(server)
+
+    Dot.playerId = server['player_turn_id']
+    YO=server['player_turn_id']
+
+    if(YO==1):
+        OPONENTE=2
+    else:
+        OPONENTE=1
+
+    #print("Variables test")
+    #print("YO: "+str(YO))
+    #print("OPONENTE: "+str(OPONENTE))
+
+
+    Dot.gameId = server['game_id']
+
+    Dot.board=server['board']
+
+    #movementType = int(input("0: Horizontal\n1: Vertical\n\t"))
+    #movementIndex = int(input("0 - 29: "))
+    #movementType=random.randint(0, 1)
+    #movementIndex=random.randint(0, 29)
+
+
+    data_tree = GameTree(int(YO))
+    data_tree.armarArbol(Dot.board[0],Dot.board[1],1)
+
+    alpha=AlphaBeta(data_tree)
+    turno=alpha.alpha_beta_search(alpha.root)
+
+    
+    while int(Dot.board[turno[0]][turno[1]]) != 99:
+        print("Entro al random")
+    	
+	
+    sio.emit('play', {
+        'player_turn_id': Dot.playerId,
+        'tournament_id': Dot.tournamentId,
+        'game_id': Dot.gameId,
+        'movement': turno
+    })
+    print("Mando")
+
+@sio.on('finish')
+def on_finish(server):
+
+
+    if (server['player_turn_id'] == server['winner_turn_id']):
+        print("Win")
+    else:
+        print("Loose")
+    
+    sio.emit('player_ready', {
+        'tournament_id': Dot.tournamentId,
+        'game_id': Dot.gameId,
+        'player_turn_id': Dot.playerId
+    })
+    
+    Dot.restart()
+
+#print(sys.argv)
+
+'''
+host
+tid
+nombre
+'''
+
+Dot=DotsAndBoxes(sys.argv[3],sys.argv[2])
+sio.connect(sys.argv[1])
+
+		
---
+++ con MiniMax/Entrega/generarArbolTest.py
@@ -0,0 +1,272 @@
+""" 
+Jose Cifuentes
+
+"""
+import random
+
+N = 6 #cantidad de puntos en una linea
+
+#YO=1 #Jugadas positivas
+#OPONENTE=2 #Jugadas negativas
+
+EMPTY=99
+FILLED=0
+
+def getPosicionesLibres(lista):
+	posicionesLibres=[]
+	for i in range(len(lista)):
+		if(lista[i]==99):
+			posicionesLibres.append(i)
+	return posicionesLibres
+		
+
+
+
+
+def buscarTiroCuadrado(tableroH,tableroV):
+	posicionesLibresH=getPosicionesLibres(tableroH)
+	posicionesLibresV=getPosicionesLibres(tableroV)
+
+	posiblesTiros=[]
+
+	for pos in posicionesLibresH:
+		posiblesTiros.append([0,pos])
+
+	for pos in posicionesLibresV:
+		posiblesTiros.append([1,pos])
+
+	#print(posiblesTiros)
+	
+
+	for turno in posiblesTiros:
+
+		punteoAntesTurno=0
+		punteoTurno=0
+
+		acumulador = 0
+		contador = 0
+
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoAntesTurno = punteoAntesTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(turno[0]==0):
+			#Horizontal
+			tableroH[turno[1]]=FILLED
+		else:
+			#Vertical
+			tableroV[turno[1]]=FILLED
+
+
+		acumulador = 0
+		contador = 0
+
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoTurno = punteoTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(punteoAntesTurno<punteoTurno):
+
+			#print("Cuadrado Interno")
+
+			return turno
+
+		else:
+
+			if(turno[0]==0):
+				#Horizontal
+				tableroH[turno[1]]=EMPTY
+			else:
+				#Vertical
+				tableroV[turno[1]]=EMPTY
+
+
+	if(len(posiblesTiros)>0):
+		return random.choice(posiblesTiros)
+	else:
+		return [0,0]
+
+
+def jugarSimulado(tableroH,tableroV,turno,jugador):
+	repetirTurno=True
+	contador=0
+
+	while repetirTurno:
+
+		if(contador!=0):
+			#print("Se podria simular otro tiro")
+			turno=buscarTiroCuadrado(tableroH.copy(),tableroV.copy())
+			#print("Turno ganador: ")
+			#print(turno)
+			
+		else:
+			contador+=1
+
+		punteoAntesTurno=0
+		punteoTurno=0
+
+		acumulador = 0
+		contador = 0
+
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoAntesTurno = punteoAntesTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(turno[0]==0):
+			#Horizontal
+			tableroH[turno[1]]=FILLED
+		else:
+			#Vertical
+			tableroV[turno[1]]=FILLED
+
+
+		acumulador = 0
+		contador = 0
+
+		for x in range(0, N * (N - 1)):
+			if((x+1) % N) != 0:
+				if tableroH[x] != EMPTY and tableroH[x+1] != EMPTY and tableroV[contador + acumulador] != EMPTY and tableroV[contador + acumulador + 1] != EMPTY:
+					punteoTurno = punteoTurno + 1
+					
+				acumulador = acumulador + N
+			else:
+				contador = contador + 1
+				acumulador = 0
+
+		if(punteoAntesTurno<punteoTurno):
+			#print()
+			#print("Jugada con cuadrado")
+			#print("Tablero:")
+			#print([tableroH,tableroV])
+			#print()
+			repetirTurno=True
+
+			if(jugador==1):
+				#Positivos
+				if(punteoTurno - punteoAntesTurno==1):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=1
+					else:
+						#Vertical
+						tableroV[turno[1]]=1
+
+				elif(punteoTurno - punteoAntesTurno==2):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=2
+					else:
+						#Vertical
+						tableroV[turno[1]]=2
+
+			else:
+				#Negativo
+				if(punteoTurno - punteoAntesTurno==1):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=-1
+					else:
+						#Vertical
+						tableroV[turno[1]]=-1
+
+				elif(punteoTurno - punteoAntesTurno==2):
+
+					if(turno[0]==0):
+						#Horizontal
+						tableroH[turno[1]]=-2
+					else:
+						#Vertical
+						tableroV[turno[1]]=-2
+		
+		else:
+			repetirTurno=False
+
+	return tableroH,tableroV
+
+def getHeuristica(tableroH,tableroV,jugador):
+	punteo=0
+	if(jugador==1):
+		#positivos
+		#print("Es positivos")
+		for i in tableroH:
+			if(i!=EMPTY and i>0):
+				punteo+=i
+
+		for i in tableroV:
+			if(i!=EMPTY and i>0):
+				punteo+=i
+
+		return punteo
+
+	else:
+		#Negativos
+		#print("Es Negativo")
+		for i in tableroH:
+			if(i!=EMPTY and i<0):
+				punteo+=i*-1
+
+		for i in tableroV:
+			if(i!=EMPTY and i<0):
+				punteo+=i*-1
+				
+		return punteo
+
+
+def simularMiTiro(tableroH,tableroV):
+
+
+	posicionesLibresH=getPosicionesLibres(tableroH)
+	posicionesLibresV=getPosicionesLibres(tableroV)
+
+	posiblesTiros=[]
+
+	for pos in posicionesLibresH:
+		posiblesTiros.append([0,pos])
+
+	for pos in posicionesLibresV:
+		posiblesTiros.append([1,pos])
+
+	
+	for tiro in posiblesTiros:
+		#print()
+		#print("TIRO "+str(tiro))
+		tableroHNuevo,tableroVNuevo=jugarSimulado(tableroH.copy(),tableroV.copy(),tiro,YO)
+		#print([tableroHNuevo,tableroVNuevo])
+
+		#print("punteo")
+		#print(getHeuristica(tableroHNuevo.copy(),tableroVNuevo.copy(),YO))
+
+	
+
+############
+#### MAIN ##
+############
+'''
+h=[99, 0,0,0,0,0]
+v=[99,99, 0,0, 99,0]
+
+print()
+print("SIMULAR MI TIRO")
+simularMiTiro(h.copy(),v.copy())
+'''
